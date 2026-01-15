1edb85cf18d6de19cfe086c35820b8d4a0bd4795
aayramdan05 <aayramdan@gmail.com>
2026-01-15 13:03:49 +0700

    Add API Management

--- app/Models/Cctv.php
+++ app/Models/Cctv.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo; // Import Wajib
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Cctv extends Model
 {
